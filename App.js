974b6f81d70eee753c401e2d1e741ed5b3007815
feliasdager <feliasdager@gmail.com>
2025-10-20 15:27:45 -0400

    working on user sessions

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
 import express from 'express' 
+import "dotenv/config";
+import mongoose from 'mongoose';
+import UserRoutes from "./Kanbas/Users/routes.js"; 
+import session from 'express-session';
+
 import Hello from './Hello.js'
 import Lab5 from './Lab5/index.js' 
 import cors from "cors"; 
@@ -7,9 +12,37 @@ import ModuleRoutes from './Kanbas/Modules/routes.js';
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 
 
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
+
 const app = express() 
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000"
+}));
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(
+  session(sessionOptions)
+);
+
+
 app.use(express.json()); 
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app); 
 AssignmentRoutes(app);
