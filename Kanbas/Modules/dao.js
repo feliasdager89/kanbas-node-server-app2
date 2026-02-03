873adc5653c06bdf295a403336964fafc2feba01
feliasdager <feliasdager@gmail.com>
2026-02-02 21:13:51 -0500

    updated the crud functionalities for the modules backend

---
+++ Kanbas/Modules/dao.js
@@ -0,0 +1,23 @@
+import model from "./model.js" 
+import mongoose from "mongoose" 
+
+export const createModule = (module)=>{
+    delete module._id;
+    return model.create(module);
+} 
+
+export const findAllModules = () => model.find(); 
+
+export const findModuleById = (id) => {
+    if(!mongoose.Types.ObjectId.isValid(id)) return null
+    return model.findById(id);
+} 
+
+export const findModuleByName = (name) => {
+    return model.findOne({ name: name });
+} 
+
+export const updateModule = (id, module) => model.updateOne({ _id: id }, { $set: module });
+
+export const deleteModule = (id) => model.deleteOne({ _id: id }); 
+
--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -1,6 +1,43 @@
-import db from "../Database/index.js";
-export default function ModuleRoutes(app) {
+//import db from "../Database/index.js"; 
+import * as dao from "./dao.js" 
 
+export default function ModuleRoutes(app) { 
+
+  const createModule = async (req, res) => {
+    const module = await dao.createModule(req.body);
+    res.json(module);
+  }
+
+  const findAllModules = async (req, res) => {
+    const modules = await dao.findAllModules();
+    res.json(modules);
+  }
+
+  const findModuleById = async (req, res) => {
+    const { mid } = req.params;
+    const module = await dao.findModuleById(mid);
+    res.json(module);
+  }
+
+  const updateModule = async (req, res) => {
+    const { mid } = req.params;
+    await dao.updateModule(mid, req.body);
+    res.sendStatus(204);
+  }
+
+  const deleteModule = async (req, res) => {
+    const { mid } = req.params;
+    await dao.deleteModule(mid);
+    res.sendStatus(200);
+  } 
+
+  app.get("/api/modules", findAllModules);
+  app.get("/api/modules/:mid", findModuleById);
+  app.post("/api/modules", createModule);
+  app.put("/api/modules/:mid", updateModule);
+  app.delete("/api/modules/:mid", deleteModule);
+
+  /*
   app.get("/api/courses/:cid/modules", (req, res) => {
     const { cid } = req.params;
     const modules = db.modules.filter((m) => m.course === cid);
@@ -36,4 +73,5 @@ export default function ModuleRoutes(app) {
     });
 
 } 
-
+*/
+}
