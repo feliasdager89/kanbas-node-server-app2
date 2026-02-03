4ec9ce442523718f5dee71c63adbbedd9419b30d
feliasdager <feliasdager@gmail.com>
2026-02-02 21:27:01 -0500

    made a minor change

--- Kanbas/Modules/model.js
+++ Kanbas/Modules/model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import schema from "./schema.js"; 
-const model = mongoose.model("Module", schema); 
+const model = mongoose.model("ModuleModel", schema); 
 export default model; 
 
 
